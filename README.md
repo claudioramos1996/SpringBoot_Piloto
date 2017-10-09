331221141df76b003e42d3f04365079503283d2d
Claudio Ramos de Oliveira <claudio.ramosti@yahoo.com.br>
2017-10-09 11:36:47 -0300

    Fontes Iniciais e arquivo README.md para o gitHub

---
+++ README.md
@@ -0,0 +1,2 @@
+# Praticando Spring Boot
+Projeto piloto para praticar Spring Boot, também pode serve para aqueles que quiserem vê uma aplicação Spring Boot simples.
---
+++ src/main/java/br/eti/claudioramos/aprendizagem/SpringBoot/Configuracao.java
@@ -0,0 +1,15 @@
+package br.eti.claudioramos.aprendizagem.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Configuracao {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Configuracao.class, "");
+		
+	}
+
+}
